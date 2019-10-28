0544bfa21f2120f6cd9a0826b0ce97051af6f013
John Carbajal <johnxjuanito@gmail.com>
2019-10-28 08:46:54 -0700

    match

---
+++ main.js
@@ -0,0 +1,74 @@
+var Deck = [
+  {
+    card: 1,
+    value: "A"
+  },
+  {
+    card: 2,
+    value: "A"
+  },
+  {
+    card: 3,
+    value: "B"
+  },
+  {
+    card: 4,
+    value: "B"
+  },
+  {
+    card: 5,
+    value: "C"
+  },
+  {
+    card: 6,
+    value: "C"
+  },
+  {
+    card: 7,
+    value: "D"
+  },
+  {
+    card: 8,
+    value: "D"
+  },
+  {
+    card: 9,
+    value: "E"
+  },
+  {
+    card: 10,
+    value: "E"
+  },
+  {
+    card: 11,
+    value: "F"
+  },
+  {
+    card: 12,
+    value: "F"
+  },
+  {
+    card: 13,
+    value: "G"
+  },
+  {
+    card: 14,
+    value: "G"
+  },
+  {
+    card: 15,
+    value: "H"
+  },
+  {
+    card: 16,
+    value: "H"
+  },
+  {
+    card: 17,
+    value: "I"
+  },
+  {
+    card: 18,
+    value: "I"
+  }
+]
